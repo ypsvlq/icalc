9e1cef31d85c067f09c441c703c35539d9e18eea
Elaine Gibson <ypsvlq@gmail.com>
2025-12-04 14:56:41 +0000

    evaluate nested commands

--- src/input.h
+++ src/input.h
@@ -1,10 +1,14 @@
 #ifndef ICALC_INPUT_H
 #define ICALC_INPUT_H
 
+#include <stdbool.h>
+
 // uses global state: input buffer
 void input_prompt(const char *format, ...);
 
+// call input_prompt() then pass NULL for name
+//
 // uses global state: input buffer, strtok
-void input_execute(void);
+bool input_execute(double *result, const char *name);
 
 #endif
